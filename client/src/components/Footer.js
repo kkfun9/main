863db6ff221054d779899fe3964b1de0a119d3ff
kkfun9 <kkfun9@gmail.com>
2025-10-05 05:02:43 +0000

    Fix anchor href accessibility errors in Footer.js

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -15,18 +15,18 @@ const Footer = () => {
               We deliver excellence in every project with our premium products.
             </p>
             <div className="social-links">
-              <a href="#" aria-label="Facebook">
-                <i className="fab fa-facebook"></i>
-              </a>
-              <a href="#" aria-label="Twitter">
-                <i className="fab fa-twitter"></i>
-              </a>
-              <a href="#" aria-label="LinkedIn">
-                <i className="fab fa-linkedin"></i>
-              </a>
-              <a href="#" aria-label="Instagram">
-                <i className="fab fa-instagram"></i>
-              </a>
+                      <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
+                        <i className="fab fa-facebook"></i>
+                      </a>
+                      <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" aria-label="Twitter">
+                        <i className="fab fa-twitter"></i>
+                      </a>
+                      <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
+                        <i className="fab fa-linkedin"></i>
+                      </a>
+                      <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
+                        <i className="fab fa-instagram"></i>
+                      </a>
             </div>
           </div>
           
