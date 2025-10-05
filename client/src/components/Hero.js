6225c57de3b0d66a2ff99fbe1e356e64cb342318
kkfun9 <kkfun9@gmail.com>
2025-10-05 22:17:00 +0000

    Increase product image sizes and arrange as requested on home page

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -9,24 +9,30 @@ const Hero = () => {
             <h1 className="display-4 fw-bold mb-4">Our Products</h1>
             <p className="lead mb-4">High-quality construction aggregates for all your building needs</p>
             <div className="row justify-content-center">
-              <div className="col-md-2 mb-4">
-                <img src="/VelMuruganStoneCrusher.jpg" alt="VelMurugan Stone Crusher" className="img-fluid rounded shadow" />
+              {/* Line 1: Stone Crusher */}
+              <div className="col-12 mb-4">
+                <img src="/VelMuruganStoneCrusher.jpg" alt="VelMurugan Stone Crusher" style={{width: '60%', maxWidth: '600px', height: 'auto'}} className="img-fluid rounded shadow" />
                 <div className="mt-2">Stone Crusher</div>
               </div>
-              <div className="col-md-2 mb-4">
-                <img src="/newVSIcrushed20MM.jpg" alt="VSI Crushed 20MM" className="img-fluid rounded shadow" />
-                <div className="mt-2">VSI Crushed 20MM</div>
+              {/* Line 2: VSI Crushed 20MM and 12MM side by side */}
+              <div className="col-12 mb-4 d-flex justify-content-center gap-4">
+                <div>
+                  <img src="/newVSIcrushed20MM.jpg" alt="VSI Crushed 20MM" style={{width: '300px', height: 'auto'}} className="img-fluid rounded shadow" />
+                  <div className="mt-2">VSI Crushed 20MM</div>
+                </div>
+                <div>
+                  <img src="/newVSIcrushed12MM.jpg" alt="VSI Crushed 12MM" style={{width: '300px', height: 'auto'}} className="img-fluid rounded shadow" />
+                  <div className="mt-2">VSI Crushed 12MM</div>
+                </div>
               </div>
-              <div className="col-md-2 mb-4">
-                <img src="/newVSIcrushed12MM.jpg" alt="VSI Crushed 12MM" className="img-fluid rounded shadow" />
-                <div className="mt-2">VSI Crushed 12MM</div>
-              </div>
-              <div className="col-md-2 mb-4">
-                <img src="/newMsand.jpg" alt="M Sand" className="img-fluid rounded shadow" />
+              {/* Line 3: M Sand */}
+              <div className="col-12 mb-4">
+                <img src="/newMsand.jpg" alt="M Sand" style={{width: '50%', maxWidth: '400px', height: 'auto'}} className="img-fluid rounded shadow" />
                 <div className="mt-2">M Sand</div>
               </div>
-              <div className="col-md-2 mb-4">
-                <img src="/20MMvs40MM.jpg" alt="20MM vs 40MM" className="img-fluid rounded shadow" />
+              {/* Line 4: 20MM vs 40MM */}
+              <div className="col-12 mb-4">
+                <img src="/20MMvs40MM.jpg" alt="20MM vs 40MM" style={{width: '50%', maxWidth: '400px', height: 'auto'}} className="img-fluid rounded shadow" />
                 <div className="mt-2">20MM vs 40MM</div>
               </div>
             </div>
